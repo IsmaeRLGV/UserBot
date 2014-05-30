aa4cf8cb1cabf6d5ab777823183ed9f59f1f87f7
IsmaeRLGV <IsmaeRLGV@gmail.com>
2014-05-30 14:38:19 -0430

    Serializador de las Bases de Datos

---
+++ __UserHandler/db.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import logger
+
+try:
+	import cPickle as pickle
+except ImportError as e:
+	import pickle
+	logger.log(e).LogError()
+
+Dir = '/home/'+"canaima"+'/.userbot/'
+
+def save(filename, content, m_str="w"):
+	logger.log("ROUTE: " + Dir + filename + ".db" ).Logger()
+	logger.log("SAVE: %s" %  content).Logger()
+	filename = file(Dir + filename + ".db", m_str)
+	pickle.dump(content, filename)
+	filename.close()
+
+def load(filename):
+	logger.log("LOADING: " + Dir + filename + '.db').Logger()
+	return pickle.load(file(Dir + filename + '.db'))
