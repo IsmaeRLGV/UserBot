0cebf241da64a65f8d681e20983e2c1485aac7c7
IsmaeRLGV <IsmaeRLGV@gmail.com>
2014-08-07 16:30:33 -0430

    Update db.py

--- __UserHandler/db.py
+++ __UserHandler/db.py
@@ -1,21 +1,24 @@
+# -*- coding: utf-8 -*-
 
 import logger
 
 try:
-	import cPickle as pickle
+    import cPickle as pickle
 except ImportError as e:
-	import pickle
-	logger.log(e).LogError()
+    import pickle  # lint:ok
+    logger.log(e).LogError()
 
 Dir = 'DB/DB/'
 
+
 def save(filename, content, m_str="w"):
-	logger.log("ROUTE: " + Dir + filename + ".db" ).Logger()
-	logger.log("SAVE: %s" %  content).Logger()
-	filename = file(Dir + filename + ".db", m_str)
-	pickle.dump(content, filename)
-	filename.close()
+    logger.log("ROUTE: " + Dir + filename + ".db").Logger()
+    logger.log("SAVE: %s" % content).Logger()
+    filename = file(Dir + filename + ".db", m_str)
+    pickle.dump(content, filename)
+    filename.close()
+
 
 def load(filename):
-	logger.log("LOADING: " + Dir + filename + '.db').Logger()
-	return pickle.load(file(Dir + filename + '.db'))
+    logger.log("LOADING: " + Dir + filename + '.db').Logger()
+    return pickle.load(file(Dir + filename + '.db'))
