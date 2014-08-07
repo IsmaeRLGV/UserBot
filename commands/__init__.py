353849752c7bb0c033c48b778a95c08c728b5e98
IsmaeRLGV <IsmaeRLGV@gmail.com>
2014-08-07 16:21:11 -0430

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 
-
 import Google
-import UrlOpen
-import newCommand__
 import AutoModerator
-#import CommandHandler
 import wikipedia
-#import tweepy
-import translate
+import tweepy
 import logger
 import threading
 import VG
@@ -16,61 +11,61 @@ import IRC
 
 import time
 
-from __UserHandler.flags import Flags
-from newCommand__ import (newCommand, ThisCommand, ifOK)
+from __UserHandler import admin
+from newCommand import (newCommand, ThisCommand, ifOK, CommandList)
 
 # Comandos sobre el IRC
-kick = newCommand('k|kick',
+kick = newCommand('kick|k',
       'Expulsa a un usuario de un canal especifico.',
-      'k <usuario> <Comentario Opciona>')
-remove = newCommand('rm|remove',
+      'kick <usuario> <Comentario Opciona>')
+remove = newCommand('remove|rm',
         'Saca a un usuario de un canal especifico.',
-        'rm <usuario>')
+        'remove <usuario>')
 op = newCommand('op',
     'Otorga estado de operador a un usuario.',
     'op <usuario>')
 deop = newCommand('deop',
     'Quita estado de operador a un usuario.',
     'deop <usuario>')
-voice = newCommand('v|voice',
+voice = newCommand('voice|voice',
     'Otorga voz a un usuario.',
-    'v <usuario>')
-devoice = newCommand('dv|devoice',
+    'voice <usuario>')
+devoice = newCommand('devoice|dv',
     'Quita voz a un usuario.',
-    'dv <usuario> ó simplemente dv')
-ban = newCommand('b|ban',
+    'devoice <usuario> ó simplemente devoice')
+ban = newCommand('ban|b',
     'Banea a un usuario de un canal en especifico.',
     'b <nick!ident@host>')
-unban = newCommand('ub|unban',
+unban = newCommand('unban|ub',
     'Quita veto a un usuario en un canal especifico.',
     'ub <nick!ident@host>')
-part = newCommand('p|part',
+part = newCommand('part|p',
     'Hace que el Bot abandone un canal.',
      'p <canal> <Comentario Opcional>')
-join = newCommand('j|join',
+join = newCommand('join|j',
     'Hace que el Bot ingrese a un canal.',
     'j <canal> <Clave Opcional>')
 
 # Comandos sobre los Servicios
-flags = newCommand('fl|flags',
-    'Cambia los) Flags de un usuario via ChanServ',
-    'flags <usuario> <(+|-)FLAGS>')
-Set = newCommand('st|set',
+flags = newCommand('flags|fl',
+    'Cambia los Flags de un usuario via ChanServ',
+    'flags <usuario> <(+|-)<FLAGS>')
+Set = newCommand('set|st',
     'permite configurar indicadores de control de canales via ChanServ',
     'set <Target> (on|off)')
 
 # Otros comandos.
-traducir = newCommand('tr|traducir',
+traducir = newCommand('traducir|translate|tr',
     'Traduce una frase de un idioma a otro.',
     'tr <in> <out> <Frase>')
-imagen = newCommand('img|imagen',
+imagen = newCommand('imagen|img',
     'Busca una imagen de un Objeto Especificado.',
     'img <Objeto>')
-google = newCommand('gl|google',
-    'Realiza una busqueda en google de un Objeto Especificado.',
+google = newCommand('google|gl',
+    'Realiza una busqueda en google de algo en concreto.',
     'gl <Objeto>')
 wiki = newCommand('wiki|wikipedia',
-    'Realiza una busqueda en wikipedia de un Objeto Especificado.',
+    'Realiza una busqueda en wikipedia de algo en concreto.',
     'wiki <Objeto>')
 ip = newCommand('ip',
     'Geolocaliza una ip.',
@@ -81,6 +76,12 @@ twitter = newCommand('tweet|twitter',
 endis = newCommand('ctrl|control',
     'Regula o controla el uso de algunos comandos en un canal.',
     'control <Comando> on|off')
+ayuda = newCommand('ayuda|help',
+    'muestra la ayuda disponible de un comando.',
+    'ayuda <Comando>')
+red = newCommand('red|irc',
+    'Establece o desconecta una conexion a algun servidor IRC.',
+    'red add|del <server>:<port>:<sasluse>')
 
 wikipedia.set_lang('es')
 
@@ -99,16 +100,17 @@ class MainLoop(threading.Thread):
             sc = self.SocketObject.get()
             Client = IRC.client.cliente(sc)
             args = self.Out_Queue.get().split()
+            #print 'Commands -> ' + ' '.join(args[1:])
             data = args[0].replace('!', ' ').replace('@', ' ').split()
             # Procesando Comandos Sobre el IRC
             try:
                 if ThisCommand(kick, args[1]):
                     if ifOK(data, 'k', sc):
-                        if args[2] != VG.nick:
+                        if args[2] != VG.IC['NICK']:
                             Client.kick(data[1], args[2], ' '.join(args[3:]))
                 if ThisCommand(remove, args[1]):
                     if ifOK(data, 'k', sc):
-                        if args[2] != VG.nick:
+                        if args[2] != VG.IC['NICK']:
                             Client.remove(data[1], args[2], ' '.join(args[3:]))
                 if ThisCommand(op, args[1]):
                     if ifOK(data, 'o', sc):
@@ -142,40 +144,54 @@ class MainLoop(threading.Thread):
                     if ifOK(data, 'c', sc):
                         Client.Join(args[2], ' '.join(args[3:]))
                 if ThisCommand(endis, args[1]):
-                    if ifOK(data, 'S', sc):
-                        try:
-                            for j in eval('VG.%s' % args[2]).keys():
-                                if j.match(data[1]):
-                                    raise StopIteration
-                        except StopIteration:
-                            h={'on': '\00303on', 'off': '\00304off'}
-                            if args[3] in ['on', 'off']:
-                                eval('VG.%s' % args[2])[j] = args[3]
-                                Client.notice(data[1], '\2\00301%s %s' % (args[2], h[args[3]]))
-                        except NameError:
-                            Client.notice(data[1], '\00304ERROR\3:%s No existe.' % args[2])
-                        else:
-                            Client.notice(data[1], '\00304ERROR\3: ¿El canal no esta registrado?')
+                    try:
+                        for j in eval('VG.%s' % args[2]).keys():
+                            if j.match(data[1]):
+                                raise StopIteration
+                    except StopIteration:
+                        h={'on': '\00303on', 'off': '\00304off'}
+                        if args[3] in ['on', 'off']:
+                            eval('VG.%s' % args[2])[j] = args[3]
+                            Client.notice(data[1], '\2\00301%s %s' % (args[2], h[args[3]]))
+                    except NameError:
+                        Client.notice(data[1], '\00304ERROR\3:%s No existe.' % args[2])
+                    else:
+                        Client.notice(data[1], '\00304ERROR\3: ¿El canal no esta registrado?')
                 if ThisCommand(wiki, args[1]):
                     try:
                         a='\2\00301%s\2\3: %s' % (args[2], wikipedia.summary(args[2], 1))
                         Client.notice(data[1], a.encode('UTF-8'))
                     except wikipedia.exceptions.PageError:
-                        a=('\00304ERROR\3: Pruebe con -> ' +
-                          '\2\00304, \2\3'.join(wikipedia.search(args[2])))
-                        Client.notice(data[1], a.encode('UTF-8'))
+                        Client.notice(data[1], '\00304ERROR\3: Pruebe con -> ' +
+                        "\2\00304, \2\3".join(wikipedia.search(args[2])))
                     except:
                         Client.notice(data[1], 'Ohh Internet lento :(')
+                if ThisCommand(ayuda, args[1]):
+                    try:
+                        commandls = {}
+                        for i in CommandList:
+                            commandls[i[1]] = i[2]
+                        Client.notice(data[1], commandls.get(args[2],
+                        '\00304ERROR\3: El comando no existe'))
+                    except IndexError:
+                        pr = 'IRCBot. \2UserBot-3.9.7\2 by Kwargs.'
+                        pr += ' Inicio de comandos: %s' % VG.comd
+                        pr += '. Comandos disponibles: '
+                        Client.notice(data[1], pr)
+                        Client.notice(data[1], ', '.join(commandls.keys()))
+                        Client.notice(data[1], '\2Fin de la ayuda.')
                 if ThisCommand(traducir, args[1]):
-                    a = translate.translate(' '.join(args[4:]), args[2], args[3])
-                    Client.notice(data[1],
-                    'Traducción del %s al %s: %s' % (a[1].encode('UTF-8'),
-                    args[3], a[0].encode('UTF-8')))
+                    Client.notice(data[1], Google.translate(' '.join(args[4:]), args[2], args[3]).encode('UTF-8')
+                if ThisCommand(red, args[1]):
+                    info = args[3].split(':')
+                    if args[2] == 'add':
+                        IRC.connection.Loop_sc(info[0], int(info[1]), bool(info[2])).start()
+
             except IndexError:
                 Client.notice(data[0], "\00304ERROR\3: Faltan parametros")
-            #except:
-                #Client.notice(data[1],
-                #"\2\00304ERROR FATAL\3\2: Sistema de Comandos")
+            except:
+                Client.notice(data[1],
+                "\2\00304ERROR FATAL\3\2: Sistema de Comandos")
             try:
                 for i in VG.AntiFlood.keys():
                     if i.match(data[1]):
@@ -184,7 +200,7 @@ class MainLoop(threading.Thread):
                 if VG.AntiFlood[i] == 'on':
                     t = (af['UR'] - time.time()) * 1
                     if t < af['TM'] and af['MR'] >= af['MM'] and data[0] == af['UU']:
-                        Client.kick(data[1], data[0], 'Deja el Flood!!!')
+                        Client.kick(data[1], data[0], '\2\00300,04ALTO AL FLOOD')
                     elif t > af['TM'] and af['MR'] <= af['MM'] and data[0] == af['UU']:
                         af['UR'] = time.time()
                         af['MR'] = 1
